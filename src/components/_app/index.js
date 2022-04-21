a898bce86e3335f2d97f753a543338a997a9aa15
jongseung park <sa96125@gmail.com>
2022-04-21 23:13:16 +0900

    🔥 Remove Wapper component
    # 불필요한 레이아웃 스타일 제거

--- src/components/_app/index.js
+++ src/components/_app/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Routes from '../../router/hot-routes';
 import { useSelector } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
-import { AppWrapper } from './style';
 import Theme from '../../styles/theme';
 import GlobalStyles from '../../styles/reset';
 import Header from '../header';
@@ -13,11 +12,8 @@ const App = () => {
 	return (
 		<ThemeProvider theme={Theme}>
 			<GlobalStyles />
-			<AppWrapper>
 				<Header />
 				<Routes isSignedIn={isSignedIn} />
-				{/* <Footer /> */}
-			</AppWrapper>
 		</ThemeProvider>
 	);
 };
